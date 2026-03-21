5d5882bd5f23f245070add9cc9d10a2a18b1ab6a
Parker Piedmont <pjpiedmont@gmail.com>
2026-03-20 23:40:59 -0700

    Instantiate analyzer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useAnalyserNode } from "./hooks/useAnalyserNode";
+
 function App() {
+	const { analyser, isRunning, startAnalyser, stopAnalyser } = useAnalyserNode();
+
 	return (
 		<div>
 			<h1>Audio Analyzer</h1>
+
+			<button onClick={isRunning ? stopAnalyser : startAnalyser}>
+				{isRunning ? "Stop Analyser" : "Start Analyser"}
+			</button>
+
+			{analyser && <p>Analyser is running with FFT size: {analyser.fftSize}</p>}
+			{!analyser && <p>Analyser is not running.</p>}
 		</div>
 	)
 }
