4a059da78f8dab679c066d6d299bfbd1e8d53b54
Parker Piedmont <pjpiedmont@gmail.com>
2026-03-21 19:42:13 -0700

    Draw full buffer every frame instead of just one sample

--- src/components/Waveform.tsx
+++ src/components/Waveform.tsx
@@ -11,8 +11,20 @@ export function Waveform({ analyser }: Props) {
 		if (!analyser || !canvasRef.current) return;
 
 		const canvas = canvasRef.current;
-		const canvasContext = canvas.getContext("2d");
-		const bufferLength = analyser.frequencyBinCount;
+		const canvasContext = canvas.getContext("2d", { willReadFrequently: true });
+
+		const sampleRate = analyser.context.sampleRate;
+		const bufferLength = analyser.fftSize; // samples
+		const bufferDuration = analyser.fftSize / sampleRate; // seconds
+		const windowDuration = 5; // seconds
+		const bufferWidth = (bufferDuration / windowDuration) * canvas.width; // pixels
+
+		console.log("Sample Rate (Hz):", sampleRate);
+		console.log("Buffer Length (samples):", bufferLength);
+		console.log("Buffer Duration (s):", bufferDuration);
+		console.log("Window Duration (s):", windowDuration);
+		console.log("Buffer Width (px):", bufferWidth);
+
 		const dataArray = new Uint8Array(bufferLength);
 
 		let animationId: number;
@@ -23,33 +35,42 @@ export function Waveform({ analyser }: Props) {
 
 			if (!canvasContext) return;
 
-			const imageData = canvasContext.getImageData(1, 0, canvas.width - 1, canvas.height);
+			const imageData = canvasContext.getImageData(
+				bufferWidth, 0,
+				canvas.width - bufferWidth, canvas.height
+			);
 			canvasContext.putImageData(imageData, 0, 0);
 
 			canvasContext.fillStyle = "rgb(0, 0, 0)";
-			canvasContext.fillRect(canvas.width - 1, 0, 1, canvas.height);
+			canvasContext.fillRect(
+				canvas.width - bufferWidth, 0,
+				bufferWidth, canvas.height
+			);
 
-			const x = canvas.width - 1;
-
-			let peak = 127
-			dataArray.forEach(v => {
-				if (Math.abs(v - 127) > Math.abs(peak - 127)) peak = v
+			const points = Array.from(dataArray).map((value, i) => {
+				const x = (canvas.width - bufferWidth) + (i / bufferLength) * bufferWidth;
+				const y = canvas.height - (value / 256) * canvas.height;
+				return { x, y };
 			});
 
-			const y = canvas.height - (peak / 256) * canvas.height;
-
+			canvasContext.lineWidth = 2;
+			canvasContext.strokeStyle = "rgb(255, 255, 255)";
 			canvasContext.beginPath();
-			canvasContext.moveTo(x - 1, canvas.height / 2);
-			canvasContext.lineTo(x, y);
+
+			points.forEach(({ x, y }, i) => {
+				if (i === 0) {
+					canvasContext.moveTo(x, y);
+				} else {
+					canvasContext.lineTo(x, y);
+				}
+			});
+
 			canvasContext.stroke();
 		};
 
 		if (canvasContext) {
 			canvasContext.fillStyle = "rgb(0, 0, 0)";
 			canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
-			canvasContext.lineWidth = 2;
-			canvasContext.strokeStyle = "rgb(255, 255, 255)";
 		}
 
 		draw();
