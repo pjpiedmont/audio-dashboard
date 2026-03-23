3e6d22c6586e370400cbf4da8816a356acb1e572
Parker Piedmont <pjpiedmont@gmail.com>
2026-03-22 18:08:35 -0700

    Switch to grid layout for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -15,29 +15,18 @@ function App() {
 	} = useAnalyserNode(4096);
 
 	return (
-		<div>
-			<div className="container">
-				<h1>Audio Analyzer</h1>
+		<div className="container">
+			<h1>Audio Analyzer</h1>
 
-				<button onClick={isRunning ? stopAnalyser : startAnalyser}>
-					{isRunning ? "Stop Analyser" : "Start Analyser"}
-				</button>
+			<button onClick={isRunning ? stopAnalyser : startAnalyser}>
+				{isRunning ? "Stop Analyser" : "Start Analyser"}
+			</button>
 
-				{analyser && <p>Analyser is running with FFT size: {analyser.fftSize}</p>}
-				{!analyser && <p>Analyser is not running.</p>}
+			{analyser && <p>Analyser is running with FFT size: {analyser.fftSize}</p>}
+			{!analyser && <p>Analyser is not running.</p>}
 
-				{analyser && <div>
-					<Panel
-						label="Spectrum Analyzer"
-						color="--color-spectrum-high"
-					>
-						<SpectrumAnalyzer analyser={analyser} />
-					</Panel>
-
-					<Panel label="Oscilloscope" color="--color-oscilloscope">
-						<Oscilloscope analyser={analyser} />
-					</Panel>
-
+			{analyser && <div className="dashboard">
+				<div className="width-full">
 					<Panel label="Waveform" color="--color-waveform">
 						<Waveform
 							analyser={analyser}
@@ -45,9 +34,22 @@ function App() {
 							writeHead={writeHead}
 						/>
 					</Panel>
-				</div>}
-			</div>
+				</div>
+
+				<div className="width-half">
+					<Panel label="Oscilloscope" color="--color-oscilloscope">
+						<Oscilloscope analyser={analyser} />
+					</Panel>
+				</div>
+
+				<div className="width-half">
+					<Panel label="Spectrum Analyzer" color="--color-spectrum-high">
+						<SpectrumAnalyzer analyser={analyser} />
+					</Panel>
+				</div>
+			</div>}
 		</div>
+		// </div>
 	)
 }
 
