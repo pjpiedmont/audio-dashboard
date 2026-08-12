e0308fa2bf3319fd9065370b2dc2997df67590a5
Parker Piedmont <pjpiedmont@gmail.com>
2026-08-11 21:54:08 -0700

    Create class to represent a circular buffer

---
+++ src/utils/CircularBuffer.ts
@@ -0,0 +1,38 @@
+export class CircularBuffer {
+	public readonly buffer: Float32Array;
+	public readHead: number;
+	public writeHead: number;
+	public readonly size: number;
+
+	constructor(size: number) {
+		this.size = size;
+		this.buffer = new Float32Array(size);
+		this.readHead = 0;
+		this.writeHead = 0;
+	}
+
+	public write(samples: Float32Array): void {
+		samples.forEach(sample => {
+			this.buffer[this.writeHead] = sample;
+			this.writeHead++;
+			this.writeHead = this.writeHead % this.size;
+		});
+	}
+
+	public read(count: number): Float32Array {
+		const output = new Float32Array(count);
+
+		for (let i = 0; i < count; i++) {
+			output[i] = this.buffer[this.readHead];
+			this.readHead++;
+			this.readHead = this.readHead % this.size;
+		}
+
+		return output;
+	}
+
+	public clear(): void {
+		this.readHead = 0;
+		this.writeHead = 0;
+	}
+}
